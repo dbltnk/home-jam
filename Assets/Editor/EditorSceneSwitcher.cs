4aea8f875d811fc9042755684a1da4073b33dd76
Sebastian Dorda <hagish@schattenkind.net>
2022-05-07 14:06:25 +0200

    Integrated art assets

---
+++ Assets/Editor/EditorSceneSwitcher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public static class EditorSceneSwitcher {
+	[MenuItem("Scenes/SampleScene")]
+	static void Open0()
+	{
+		EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+	}
+
+	[MenuItem("Scenes/Prefabs")]
+	static void Open1()
+	{
+		EditorSceneManager.OpenScene("Assets/Scenes/Prefabs.unity");
+	}
+}
