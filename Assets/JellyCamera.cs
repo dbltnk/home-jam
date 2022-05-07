7314d82097f825c4407970f2f4e4e5a5c8382dad
Sebastian Dorda <hagish@schattenkind.net>
2022-05-07 13:10:29 +0200

    Camera rotaiton controlls

--- Assets/JellyCamera.cs
+++ Assets/JellyCamera.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class JellyCamera : MonoBehaviour
@@ -6,11 +5,24 @@ public class JellyCamera : MonoBehaviour
     [SerializeField] private GameObject Target;
     [SerializeField] private float Distance;
     [SerializeField] private float Height;
+    [SerializeField] private float RotationSpeed;
+
+    private JellyInputs Inputs;
+    
+    private void Awake()
+    {
+        Inputs = new JellyInputs();
+        Inputs.Enable();
+    }
 
     private void LateUpdate()
     {
-        var forwardOnPlane = Vector3.ProjectOnPlane(Target.transform.forward, Vector3.up).normalized;
-        var offset = -forwardOnPlane * Distance + Vector3.up * Height; 
+        var rotate = Inputs.Player.Move.ReadValue<Vector2>();
+        var rotDelta = rotate.x * RotationSpeed * Time.deltaTime;
+        transform.Rotate(0f, rotDelta, 0f, Space.World);
+        
+        var forwardOnPlane = Vector3.ProjectOnPlane(transform.forward, Vector3.up).normalized;
+        var offset = -forwardOnPlane * Distance + Vector3.up * Height;
         transform.position = Target.transform.position + offset;
     }
 }
