7d791cf9cdd7163ae8a673080c2db3a75dd822a7
Alexander Zacherl <alexander.zacherl@googlemail.com>
2022-05-07 16:14:31 +0100

    magnetism

--- Assets/Scripts/ChaosCounter.cs
+++ Assets/Scripts/ChaosCounter.cs
@@ -8,6 +8,10 @@ public class ChaosCounter : MonoBehaviour
     ChaosObject[] chaosObjects;
     public float Chaos;
     public TMP_Text text;
+    private JellyController jellyController;
+
+    public float AttractionDistance = 5f;
+    public float AttractionForce = 4f;
 
     void Awake() {
         text = GetComponent<TMP_Text>();
@@ -16,6 +20,7 @@ public class ChaosCounter : MonoBehaviour
     void Start()
     {
         chaosObjects = FindObjectsOfType<ChaosObject>();
+        jellyController = FindObjectOfType<JellyController>();
     }
 
     void Update()
@@ -26,6 +31,12 @@ public class ChaosCounter : MonoBehaviour
             if (chaosObject.ContributesToChaos) {
                 Chaos += chaosObject.PosChaos;// + chaosObject.RotChaos;
             }     
+
+            if (!chaosObject.Carried) {
+                Vector3 dirToJelly = (jellyController.transform.position - chaosObject.transform.position);       
+                float distanceToJelly = dirToJelly.magnitude;
+                if (distanceToJelly < AttractionDistance) chaosObject.GetComponent<Rigidbody>().AddForce(dirToJelly * AttractionForce);
+            }
         }
 
         text.text = "Chaos: " + Chaos.ToString("0");
