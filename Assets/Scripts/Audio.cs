f6df65568b35de8aeca97670a1d7375ce0bdb167
Sebastian Dorda <hagish@schattenkind.net>
2022-05-08 10:25:11 +0200

    audio import button and random substring play

--- Assets/Scripts/Audio.cs
+++ Assets/Scripts/Audio.cs
@@ -1,28 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using Random = UnityEngine.Random;
 
 public class Audio : MonoBehaviour
 {
     public static Audio Instance;
+
+    [SerializeField] private string ImportPath = "Audio";
 
     private void Awake()
     {
         Instance = this;
     }
 
+    private IEnumerable<Transform> EnumMatchingChildren(string name)
+    {
+        foreach (Transform t in transform)
+        {
+            if (Utils.FuzzyMatch(t.name, name))
+            {
+                yield return t;
+            }
+        }
+    }
+    
     public void PlayAt(string name, Vector3 pos)
     {
-        var group = transform.Find(name);
-        if (group == null) return;
+        var possibleNodes = EnumMatchingChildren(name).ToList();
+        if (possibleNodes.Count == 0) return;
+        var group = Utils.PickRandom(possibleNodes);
         var readySources = group.GetComponentsInChildren<AudioSource>()
             .Where(it => it.isPlaying == false)
             .ToList();
         if (readySources.Count > 0)
         {
-            int idx = Random.Range(0, readySources.Count);
-            var source = readySources[idx];
+            var source = Utils.PickRandom(readySources);
             source.transform.position = pos;
             source.Play();
         }
     }
+
+    #if UNITY_EDITOR
+    [Button("Import")]
+    void Import()
+    {
+        var importRoot = new GameObject("__IMPORT__");
+        importRoot.transform.SetParent(transform);
+        Debug.Log(Application.dataPath);
+        var path = Path.Combine(Application.dataPath, ImportPath);
+        foreach (var it in Directory.GetFiles(path, "*.ogg", SearchOption.AllDirectories))
+        {
+            var parts = it.Split("Assets", 2);
+            var assetPath = "Assets/" + parts[1];
+            AudioClip clip = (AudioClip)UnityEditor.AssetDatabase.LoadAssetAtPath(assetPath, typeof(AudioClip));
+            Debug.Log($"{it} {clip.name}");
+            var go = new GameObject(clip.name);
+            go.transform.SetParent(importRoot.transform);
+            go.AddComponent<AudioSource>().clip = clip;
+        }
+    }
+    #endif
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,6 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 public static class Utils
 {
     public static float MapIntoRange (float value, float from1, float to1, float from2, float to2) {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2; 
+    }
+
+    public static bool FuzzyMatch(string haystack, string needle)
+    {
+        return haystack.ToLower().Contains(needle.ToLower());
+    }
+
+    public static T PickRandom<T>(IList<T> l)
+    {
+        int idx = Random.Range(0, l.Count);
+        return l[idx];
     }
 }
